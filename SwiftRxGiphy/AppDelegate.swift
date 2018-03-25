ca4193c02d0b51afdf4ed4bc74d00f3736ff2b18
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2018-03-25 23:04:47 +0300

    Initial commit

---
+++ SwiftRxGiphy/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SwiftRxGiphy
+//
+//  Created by Vitali Kazakevich on 3/25/18.
+//  Copyright © 2018 Vitali Kazakevich. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+
+}
+
