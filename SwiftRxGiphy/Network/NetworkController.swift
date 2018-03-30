09a8583dcd469e1dba2de5e43062eb04ba230554
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2018-03-30 17:55:50 +0300

    Request builder

---
+++ SwiftRxGiphy/Network/NetworkController.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkController.swift
+//  SwiftRxGiphy
+//
+//  Created by Kazakevich, Vitaly on 3/30/18.
+//  Copyright © 2018 Vitali Kazakevich. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+
+}
---
+++ SwiftRxGiphy/Network/RequestBuilder.swift
@@ -0,0 +1,25 @@
+//
+//  RequestBuilder.swift
+//  SwiftRxGiphy
+//
+//  Created by Kazakevich, Vitaly on 3/30/18.
+//  Copyright © 2018 Vitali Kazakevich. All rights reserved.
+//
+
+import Foundation
+
+struct RequestBuilder {
+    private let apiKey = "ydW1gRhQ50p3hk74lioamIoTzIK6Clg4"
+    private let hostName = "http://api.giphy.com/v1/gifs/"
+    let requestedName: String?
+    let contentSize: Int?
+    let offset: Int?
+
+    func build() -> URLRequest? {
+        let urlString = hostName + ""
+        let url = URL(string: urlString)
+        guard let requestUrl = url else { return nil }
+
+        return URLRequest(url: requestUrl)
+    }
+}
